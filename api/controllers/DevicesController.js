f332fdf300af2880c666cf057639547190a50f30
boozikaa <khomsan.cng@gmail.com>
2014-09-12 01:40:01 +0700

    use model & controller

---
+++ api/controllers/DevicesController.js
@@ -0,0 +1,11 @@
+/**
+ * DevicesController
+ *
+ * @description :: Server-side logic for managing devices
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
